7af68dc5fb561d2dd06c27726355a224e1d03d86
hi <hamidshakeel2020@gmail.com>
2025-01-27 15:12:54 +0500

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_on_boarding/firebase_options.dart';
 import 'package:flutter_on_boarding/iintroduction_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'data/model/add_date.dart';
 
-
 bool show = true;
 
-Future<void> main() async{
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  // async; {
-    await Hive.initFlutter();
-    Hive.registerAdapter(AdddataAdapter());
-    await Hive.openBox<Add_data>('data');
-    runApp(const BudgetWallet());
-  }
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(AdddataAdapter());
+  await Hive.openBox<Add_data>('data');
+  runApp(const BudgetWallet());
+}
 // }
 
 class BudgetWallet extends StatelessWidget {
@@ -26,7 +26,6 @@ class BudgetWallet extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: IntroScreen(),
     );
   }
